9be7eae3cad0ec8985482b6d299ccc48c1943de7
sachin pandey <sachingurh@gmail.com>
2023-04-30 01:17:49 +0530

    u

---
+++ build/precache-manifest.71f8c30443b12fa8d478c1c54348af01.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "942ce10ad617509e7aa3625adcd32561",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c867280bd9101dc7dcd6",
+    "url": "/static/css/2.8f8036f3.chunk.css"
+  },
+  {
+    "revision": "5c1b5e31146f6d3594a4",
+    "url": "/static/css/main.a530b0a1.chunk.css"
+  },
+  {
+    "revision": "c867280bd9101dc7dcd6",
+    "url": "/static/js/2.3e78a817.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/static/js/2.3e78a817.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c1b5e31146f6d3594a4",
+    "url": "/static/js/main.01b61f98.chunk.js"
+  },
+  {
+    "revision": "f71f7a0c5e56480c7aca",
+    "url": "/static/js/runtime-main.753b3364.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
